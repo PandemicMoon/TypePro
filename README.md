53b68f98de61b3e3e9e29e617ce69ba7ac206381
Pandemic Moon <pandemic@moon-network.net>
2015-08-10 12:38:14 -0400

    Added args and updated documentation
    Added use of args and updated code documentation (comments).

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ Your text file editing just got easier.
 ##Downloads
  - [Windows](https://github.com/PandemicMoon/TypePro/blob/master/typepro.exe?raw=true) (.exe)
  - [Os X](https://github.com/PandemicMoon/TypePro/blob/master/typepro.jar?raw=true) (.jar)
+ 
+ ##Keyboard Shortcuts
+| Shortcut | Effect                |
+|----------|-----------------------|
+| CTRL + C | Copy                  |
+| CTRL + X | Cut                   |
+| CTRL + V | Paste                 |
+| CTRL + A | Select All            |
+| CTRL + Z | Undo                  |
+| CRTL + Y | Redo                  |
+| CRTL + N | Close File (New File) |
+| CRTL + O | Open File             |
+| CRTL + S | Save File             |
+| CRTL + Q | Save File As          |
+| CRTL + P | Print File            |
+| F5       | Insert Date And Time  |
--- src/TypePro.java
+++ src/TypePro.java
@@ -6,26 +6,22 @@ import javax.swing.filechooser.*;
 import javax.swing.border.*;
 import javax.swing.event.*;
 import java.util.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.text.*;
 import javax.swing.undo.*;
-import java.net.URL;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.*;
 import java.nio.channels.*;
 
 public class TypePro extends JFrame implements KeyListener {
   
-  private String version = "1.2.9";
+  /* define fields */
+  private String version = "1.3.0";
   private String programName = "Type Pro - Version " + version; 
-  private String fileName;
+  
+  /* visual fields */
   private JFrame frame;
   private JTextArea typeArea;
-  private String fileURI = "none";
   private ImageIcon img;
-  private String prevText;
   private JPanel statusPanel;
-  private Boolean statusBarToggle;
   private JMenuBar menu;
   private JMenu file;
   private JMenuItem open;
@@ -50,32 +46,51 @@ public class TypePro extends JFrame implements KeyListener {
   private JMenuItem help;
   private JMenuItem update;
   private JMenuItem about;
-  private UndoManager undoManager;
   
-  public TypePro() {
+  /* function fields */
+  private UndoManager undoManager;
+  private String fileName;
+  private String prevText;
+  private Boolean statusBarToggle;
+  private String fileURI = "none";
+  
+  
+  public TypePro(String[] args) {
+    //set look and feel to that of native os
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     } catch (Exception e) { 
-      JOptionPane.showMessageDialog(frame, "Setting look and feel failed. Error: \n" + e, "Error", JOptionPane.ERROR_MESSAGE);
+      JOptionPane.showMessageDialog(frame, "Setting look and feel failed. Error: \n" + e, "Error", JOptionPane.ERROR_MESSAGE); //present caught errors
     }
+    /*build the frame*/
     initFrame();
+    //check for updates
     try {
       checkForUpdate();
     } catch (Exception e) { 
-      JOptionPane.showMessageDialog(frame, "Checking for update failed. Error: \n" + e, "Error", JOptionPane.ERROR_MESSAGE);
+      JOptionPane.showMessageDialog(frame, "Checking for update failed. Error: \n" + e, "Error", JOptionPane.ERROR_MESSAGE); //present caught errors
+    }
+    //open told to open file in args, open it
+    if (args.length > 0) {
+      open(args[0]);
     }
   }
   
   public void initFrame() {
+    //make a new JFrame
     frame = new JFrame();
+    //get logo url and set logo
     URL url = this.getClass().getResource("logo.png");
     frame.setIconImage(new ImageIcon(url).getImage());
+    //set the frame title and size
     frame.setTitle(getTitle());
     frame.setSize(900, 500);
     frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+    //prevent closing without action
     frame.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-    statusBarToggle = false;
     
+    //initialize statusbar
+    statusBarToggle = false;
     statusPanel = new JPanel();
     statusPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
     frame.add(statusPanel, BorderLayout.SOUTH);
@@ -84,15 +99,15 @@ public class TypePro extends JFrame implements KeyListener {
     JLabel statusLabel = new JLabel();
     statusLabel.setHorizontalAlignment(SwingConstants.LEFT);
     statusPanel.add(statusLabel);
-    frame.add(statusPanel, BorderLayout.SOUTH);
-    
-    frame.remove(statusPanel);
     statusLabel.setText("Line: 1, Column: 0");
     
+    //initialize type area
     typeArea = new JTextArea();
     typeArea.setCaretPosition(typeArea.getDocument().getLength());
     typeArea.setDragEnabled(true);
     typeArea.addKeyListener(this);
+    
+    //initialize and setup undoManager
     undoManager = new UndoManager();
     typeArea.getDocument().addUndoableEditListener(new UndoableEditListener() {
       @Override
@@ -103,6 +118,7 @@ public class TypePro extends JFrame implements KeyListener {
       }
     });
     
+    //initialize menubar components
     menu = new JMenuBar();
     file = new JMenu("File");
     open = new JMenuItem("Open             \tCtrl + O");
@@ -128,6 +144,7 @@ public class TypePro extends JFrame implements KeyListener {
     update = new JMenuItem("Update");
     about = new JMenuItem("About");
     
+    //add actions for each menu item
     exit.setMnemonic(KeyEvent.VK_E);
     exit.setToolTipText("Exit Type Pro");
     exit.addActionListener(new ActionListener() {
@@ -329,6 +346,7 @@ public class TypePro extends JFrame implements KeyListener {
       }
     });
     
+    //add each menu item to the menu, then add that menu to the bar
     file.add(open);
     file.add(saveAs);
     file.add(save);
@@ -362,8 +380,10 @@ public class TypePro extends JFrame implements KeyListener {
     info.add(about);
     menu.add(info);
     
+    //add the menu bar to the frame
     frame.setJMenuBar(menu);
     
+    //find and pass the position of the caret to the statusbar
     typeArea.addCaretListener(new CaretListener() {
       public void caretUpdate(CaretEvent e) {
         JTextArea editArea = (JTextArea)e.getSource();
@@ -384,13 +404,15 @@ public class TypePro extends JFrame implements KeyListener {
         statusLabel.setText("Line: " + linenum + ", Column: " + columnnum);
       }
     });
-
-
+    
+    //initialize scrollpane
     JScrollPane scrollPane = new JScrollPane(typeArea,
     JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
     JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
     
+    //add scrollpane
     frame.add(scrollPane);
+    //add closing event
     frame.addWindowListener(new WindowAdapter() {
       
       public void windowClosing(WindowEvent e) {
@@ -411,12 +433,13 @@ public class TypePro extends JFrame implements KeyListener {
         typeArea.requestFocusInWindow();
       }
     });
-
-
+    
+    //show the frame now that's it's been built
     frame.setVisible(true);
   }
   
   public void checkForUpdate() throws Exception {
+    //get latest version
     URL file = new URL("https://raw.githubusercontent.com/PandemicMoon/TypePro/master/src/TypePro.java");
     BufferedReader in = new BufferedReader(new InputStreamReader(file.openStream()));
     
@@ -427,113 +450,123 @@ public class TypePro extends JFrame implements KeyListener {
         versionLine = inputLine;
     }
     in.close();
+    //check if latest update is newer
     if (versionLine != null) {
       versionLine = versionLine.substring(versionLine.indexOf("\"")+1);
       String ver = versionLine.substring(0, versionLine.indexOf("\""));
       if (isNewerVersion(ver))
-        update();
+        update(); //it is, update
     } else {
-      JOptionPane.showMessageDialog(frame, "Could not check latest version number.", "Error", JOptionPane.ERROR_MESSAGE);
+      JOptionPane.showMessageDialog(frame, "Checking for update failed. Could not check latest version number.", "Error", JOptionPane.ERROR_MESSAGE); //present caught errors
     }
   }
   
+  //save file
   public void save(Boolean override) {
     String name = fileURI;
     Boolean changed = false;
+    //ask user where to save if no location found
     if (name.equals("none") || override == true) {
       String save = saveAs();
-      //System.out.println(save);
       if (save != null) {
         name = save;
         changed = true;
       }
-      /*name = JOptionPane.showInputDialog(frame, "Save File As (.txt will be added to end)", "Save", 
-                                         JOptionPane.INFORMATION_MESSAGE);
-      name = name + ".txt";*/
+      //update title
       frame.setTitle(getTitle());
     } 
+    //once again check if location is found
     if (!name.equals("none")) {
       try {
         File f = new File(name);
-        if(!f.exists() && !f.isDirectory()) { 
+        if(!f.exists() && !f.isDirectory()) { //if file doesn't exist, make it
           f.createNewFile();
-        } else if (changed == true && !sameText()) {
+        } else if (changed == true && !sameText()) { //warn user file exists
           Object[] options = { "Yes", "No" };
           int selected = JOptionPane.showOptionDialog(frame, "This file already exists.\nDo you wish to override it?", 
                                                       "Save", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, 
                                                       null, options, options[0]);
-          if (selected == 1) {
+          if (selected == 1) { //if no selected, exit method
             return;
           }
         }
+        //actual saving
         BufferedWriter out = new BufferedWriter(new FileWriter(name));
         typeArea.write(out);
         out.close();
       } catch (Exception e) { 
-        JOptionPane.showMessageDialog(frame, "Saving failed. Error: \n" + e, "Error", JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(frame, "Saving failed. Error: \n" + e, "Error", JOptionPane.ERROR_MESSAGE); //present caught errors
       }
+      //update fileURI and prevtext
       fileURI = name;
       prevText = typeArea.getText();
     }
   }
   
+  //open file
   public void open() {
-    JFileChooser chooser = new JFileChooser();
+    //save if needed
     if (!typeArea.getText().equals("") && typeArea.getText() != null)
       save(false);
-    typeArea.setText("");
+    //show file chooser
+    JFileChooser chooser = new JFileChooser();
     FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
     chooser.setFileFilter(filter);
-    int returnVal = chooser.showOpenDialog(frame);
-    if(returnVal == JFileChooser.APPROVE_OPTION) {
-       System.out.println("You chose to open this file: " +
-            chooser.getSelectedFile().getName());
+    int choosen = chooser.showOpenDialog(frame);
+    if (chooser.getSelectedFile() != null && choosen == 0) { //make sure file was chosen
+      open(chooser.getSelectedFile().getAbsolutePath());
     }
-    /*String name = JOptionPane.showInputDialog(frame, "Open File (.txt will be added to end)", "Open", 
-                                       JOptionPane.INFORMATION_MESSAGE);*/
-    if (chooser.getSelectedFile() != null) {
-      fileURI = chooser.getSelectedFile().getAbsolutePath();
-      fileName = chooser.getSelectedFile().getName();
-      try {
-        BufferedReader in = new BufferedReader(new FileReader(fileURI));
-        String line = in.readLine();
-        while (line != null)
-        {
-          typeArea.append(line);
-          line = in.readLine();
-          if (line != null)
-            typeArea.append("\n");
-        }
-        in.close();
-      } catch (Exception e) { 
-        JOptionPane.showMessageDialog(frame, "Opening failed. Error: \n" + e, "Error", JOptionPane.ERROR_MESSAGE);
+  }
+  
+  public void open(String fileloc) {
+    //clear type area
+    typeArea.setText("");
+    File f = new File(fileloc);
+    fileURI = fileloc;
+    fileName = f.getName();
+    try {
+      //actual file opening
+      BufferedReader in = new BufferedReader(new FileReader(fileURI));
+      String line = in.readLine();
+      while (line != null)
+      {
+        typeArea.append(line);
+        line = in.readLine();
+        if (line != null)
+          typeArea.append("\n");
       }
-      frame.setTitle(getTitle());
+      in.close();
+    } catch (Exception e) { 
+      JOptionPane.showMessageDialog(frame, "Opening failed. Error: \n" + e, "Error", JOptionPane.ERROR_MESSAGE); //present caught errors
     }
+    //update title
+    frame.setTitle(getTitle());
     prevText = typeArea.getText();
   }
   
+  //save as
   public String saveAs() {
+    //show chooser
     JFileChooser chooser = new JFileChooser();
     FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
     chooser.setFileFilter(filter);
     int returnVal = chooser.showSaveDialog(frame);
-    if(returnVal == JFileChooser.APPROVE_OPTION) {
-      System.out.println("You chose to save this file as: " + chooser.getSelectedFile().getName());
+    if(returnVal == JFileChooser.APPROVE_OPTION) { //make sure file was chosen
       fileName = chooser.getSelectedFile().getName();
-      return chooser.getSelectedFile().getAbsolutePath();
+      return chooser.getSelectedFile().getAbsolutePath(); //return file
     } else
-      return null;
+      return null; //return that file wasn't chosen
   }
   
   public static void main(String[] args) {
-    TypePro tPro = new TypePro();
+    TypePro tPro = new TypePro(args); //launch TypePro
   }
   
   public String getTitle() {
-    if (fileURI == null || fileURI.equals("none"))
+    //update title
+    if (fileURI == null || fileURI.equals("none")) //not editing file
       return programName;
-    else
+    else //editing file
       return fileName + " - " + programName;
   }
   
@@ -546,28 +579,31 @@ public class TypePro extends JFrame implements KeyListener {
       return false;
   }
   
+  //undo
   public void undo() {
     try {
       if (undoManager.canUndo()) {
         undoManager.undo();
       }
     } catch (CannotUndoException exp) {
-      exp.printStackTrace();
+      JOptionPane.showMessageDialog(frame, "Undo failed. Error: \n" + exp, "Error", JOptionPane.ERROR_MESSAGE); //present caught errors
     }
   }
   
+  //redo
   public void redo() {
     try {
       if (undoManager.canRedo()) {
         undoManager.redo();
       }
     } catch (CannotUndoException exp) {
-      exp.printStackTrace();
+      JOptionPane.showMessageDialog(frame, "Redo failed. Error: \n" + exp, "Error", JOptionPane.ERROR_MESSAGE); //present caught errors
     }
   }
   
+  //close file
   public void close() {
-    if (typeArea.getText() != null && !typeArea.getText().equals("")) {
+    if (typeArea.getText() != null && !typeArea.getText().equals("")) { //ask to save
       Object[] options = { "Yes", "No" };
       int selected = JOptionPane.showOptionDialog(frame, "Do you wish to save?", "Save", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
       if (selected == 0) {
@@ -581,8 +617,10 @@ public class TypePro extends JFrame implements KeyListener {
   }
   
   public void insertDateAndTime() {
+    //get date
     DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     Date date = new Date();
+    //insert date
     typeArea.insert(dateFormat.format(date), typeArea.getCaret().getMark());
   }
   
@@ -591,7 +629,9 @@ public class TypePro extends JFrame implements KeyListener {
   }
   
   public void keyPressed(KeyEvent e) {
-    System.out.println("KeyPressed("+e.getKeyCode()+") called! Control/Meta Down == " + (e.isControlDown() || e.isMetaDown()));
+    //for debugging
+    //System.out.println("KeyPressed("+e.getKeyCode()+") called! Control/Meta Down == " + (e.isControlDown() || e.isMetaDown()));
+    //add keyboard shortcuts
     if (e.getKeyCode() == KeyEvent.VK_Z && (e.isControlDown() || e.isMetaDown())) {
       undo();
     } else if (e.getKeyCode() == KeyEvent.VK_Y && (e.isControlDown() || e.isMetaDown())) {
@@ -608,7 +648,7 @@ public class TypePro extends JFrame implements KeyListener {
       try {
         typeArea.print();
       } catch (Exception ex) { 
-        JOptionPane.showMessageDialog(frame, "Printing failed. Error: /n" + e, "Error", JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(frame, "Printing failed. Error: /n" + e, "Error", JOptionPane.ERROR_MESSAGE); //present caught errors
       }
     } else if (e.getKeyCode() == KeyEvent.VK_F5) {
       insertDateAndTime();
@@ -619,10 +659,13 @@ public class TypePro extends JFrame implements KeyListener {
     //ignore
   }
   
+  //update
   public void update() {
     try {
+      //get file location to update
       final File currentFile = new File(TypePro.class.getProtectionDomain().getCodeSource().getLocation().toURI());
       URL website = null;
+      //replace (update) that file
       if (currentFile.getName().endsWith(".exe")) {
         website = new URL("https://github.com/PandemicMoon/TypePro/blob/master/typepro.exe?raw=true");
       } else if (currentFile.getName().endsWith(".jar")) {
@@ -632,34 +675,40 @@ public class TypePro extends JFrame implements KeyListener {
       FileOutputStream fos = new FileOutputStream(TypePro.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
       fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
     } catch (Exception e) {
-      JOptionPane.showMessageDialog(frame, "Update failed. Error: \n" + e, "Error", JOptionPane.ERROR_MESSAGE);
+      JOptionPane.showMessageDialog(frame, "Update failed. Error: \n" + e, "Error", JOptionPane.ERROR_MESSAGE); //present caught errors
     }
     try {
       restart();
     } catch (Exception e) {
-      JOptionPane.showMessageDialog(frame, "Failed to restart. \nError: \n" + e, "Error", JOptionPane.ERROR_MESSAGE);
+      JOptionPane.showMessageDialog(frame, "Failed to restart. \nError: \n" + e, "Error", JOptionPane.ERROR_MESSAGE); //present caught errors
     }
   }
   
   public void restart() throws Exception
   {
+    //get javaBin and location of file to restart
     final String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
     final File currentFile = new File(TypePro.class.getProtectionDomain().getCodeSource().getLocation().toURI());
     
-    /* Build command: java -jar application.jar OR java -cp application.exe TypePro */
+    //build command: java -jar application.jar [file] OR java -cp application.exe TypePro [file]
     final ArrayList<String> command = new ArrayList<String>();
     
-    /* is it a jar OR exe file? */
+    //add specific file type command
     if (currentFile.getName().endsWith(".jar")) {
       command.add(javaBin);
       command.add("-jar");
       command.add(currentFile.getPath());
+      if (fileURI != null && fileURI != "")
+        command.add(fileURI);
     } else {
       command.add(javaBin);
       command.add("-cp");
       command.add(currentFile.getPath());
       command.add("TypePro");
+      if (fileURI != null && fileURI != "")
+        command.add(fileURI);
     }
+    //build commands and exit
     final ProcessBuilder builder = new ProcessBuilder(command);
     builder.start();
     System.exit(0);
@@ -685,6 +734,7 @@ public class TypePro extends JFrame implements KeyListener {
   }
   
   public boolean isNewerVersion(String ver) {
+    //remove dots (1.0.0 --> 100)
     for (int i = 0; i < version.length(); i++) {
       if (version.substring(i, i+1).equals("."))
         version = version.substring(0, i) + version.substring(i+1);
@@ -693,8 +743,10 @@ public class TypePro extends JFrame implements KeyListener {
       if (ver.substring(i, i+1).equals("."))
         ver = ver.substring(0, i) + ver.substring(i+1);
     }
+    //convert strings to ints
     int versionint = Integer.parseInt(version);
     int verint = Integer.parseInt(ver);
+    //compare versions
     return verint > versionint;
   }
   
